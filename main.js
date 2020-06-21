57b717b0bc2490bb164de636a61d13d0de413809
0neGuyDev <0neguybusiness@gmail.com>
2020-06-21 05:26:02 +0200

    added my own encryption standard very secure
    it is top of the line, I heard tesla even uses it in their garage doors what the fuck.
    
    Also timer, it pretty cool.

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 /* Copyrighted by 0neGuyDev */
-
+console.time("it done in")
 // Congrats, you're wasting time
 // There's abosolutely no fucking reason you're here.
 // You won't get anything out of it
 // Yet you're still here
 // Whatever, do as you please
 
-var result = "010010000110010101101100011011000110111100100000010101110110111101110010011011000110010000100001"; // This will be the variable we'll be experimenting with throughout this whole waste of time, in the end it'll become "Hello World!" I know craaazy!
+var result = "010001000100100000101101010100000100110000101101010000010101001100101101010000010101001100101101010011110100100100101101001011010101011101011001001011010100111101001001001011010101010001001000001011010100000101010011001011010100011001001000001011010100101001010011"; // This will be the variable we'll be experimenting with throughout this whole waste of time, in the end it'll become "Hello World!" I know craaazy!
 
 function binaryAndStuff(string) {
 	string = string.match(/.{1,8}/g).join(" ") // If I remember regEx correctly this will add a space every eight characters.
@@ -19,6 +19,16 @@ function binaryAndStuff(string) {
 	}; return endResult.join(""); 
 }
 
+// Be prepared to see fuckery-doo
+// DH-PL-AS-AS-OI--WY-OI-TH-AS-FH-JS
+function myThing(string) {
+	return string.replace(/--/g, " ").replace(/-/g, "").replace(/DH/g, "H").replace(/PL/g, "e").replace(/AS/g, "l").replace(/OI/g, "o").replace(/WY/g, "W").replace(/TH/g, "r").replace(/FH/g, "d").replace(/JS/g, "!") // fuck you this is efficient
+	// Encryption standard = 100
+}
+
+// I don't give a shit what you say it's efficient to make it in functions and change the variable each time.
 result = binaryAndStuff(result)
+result = myThing(result)
 
 console.log(result) // This is the fucking result now fuck off.
+console.timeEnd("it done in")
