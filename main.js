59952afcd553fae1f75344bf95b8baf2c793b072
0neGuyDev <0neguybusiness@gmail.com>
2020-06-21 05:03:04 +0200

    normal stuff
    I'll be putting the script in here, nowhere else.
    
    Wait why the fuck are you looking at the commit history what the fuck.

---
+++ main.js
@@ -0,0 +1,11 @@
+/* Copyrighted by 0neGuyDev */
+
+// Congrats, you're wasting time
+// There's abosolutely no fucking reason you're here.
+// You won't get anything out of it
+// Yet you're still here
+// Whatever, do as you please
+
+var result; // This will be the variable we'll be experimenting with throughout this whole waste of time, in the end it'll become "Hello World!" I know craaazy!
+
+console.log(result) // This is the fucking result now fuck off.
