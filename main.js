7cdfdeeceb594b09ef1fbf639485c16de7c9e127
0neGuyDev <0neguybusiness@gmail.com>
2020-06-21 05:15:41 +0200

    Yayyy binary is done
    I think. I somehow got a lot of stuff right, after a few tries.

--- main.js
+++ main.js
@@ -6,6 +6,19 @@
 // Yet you're still here
 // Whatever, do as you please
 
-var result; // This will be the variable we'll be experimenting with throughout this whole waste of time, in the end it'll become "Hello World!" I know craaazy!
+var result = "010010000110010101101100011011000110111100100000010101110110111101110010011011000110010000100001"; // This will be the variable we'll be experimenting with throughout this whole waste of time, in the end it'll become "Hello World!" I know craaazy!
+
+function binaryAndStuff(string) {
+	string = string.match(/.{1,8}/g).join(" ") // If I remember regEx correctly this will add a space every eight characters.
+	
+	let bits = string.split(" ") // turn the shit into an array of BITS, vevy cooul
+	let endResult = [];
+	
+	for (let i = 0; i < bits.length; i++) {
+		endResult.push(String.fromCharCode(parseInt(bits[i], 2)))
+	}; return endResult.join(""); 
+}
+
+result = binaryAndStuff(result)
 
 console.log(result) // This is the fucking result now fuck off.
